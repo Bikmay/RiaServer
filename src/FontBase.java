f08e8c13f1b1b74e0526d1c646b0bed20e491b97
Bikmay <artyom.ivliev@gmail.com>
2017-10-18 02:47:29 +0400

    Начата работа с xml

--- src/FontBase.java
+++ src/FontBase.java
@@ -3,4 +3,10 @@ import java.util.ArrayList;
 public class FontBase {
 
     public static ArrayList<String> ListInputFonts = new ArrayList<String>();
+
+    public static ArrayList<String> GetListBaseFonts()
+    {
+        ArrayList<String> returnList = new ArrayList<String>();
+        
+    }
 }
